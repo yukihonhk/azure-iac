7094a8ea7fc0bdd9f902b1e0d2246c22775f8c12
Victor Viriya-ampanond <wviriya@microsoft.com>
2020-09-24 16:48:19 +1200

    Add action module

--- .github/actions/request-parser/request-parser.ts
+++ .github/actions/request-parser/request-parser.ts
@@ -21,7 +21,7 @@ const run = async (): Promise<void> => {
             return
         }
 
-        let appName = "", armTemplate = "", applyPolicy = false, policyName = ""
+        let appName = "", armTemplate = "", resourceGroup = ""
 
         console.log(issue.body)
         const lines = issue.body.match(/[^\r\n]+/g)
@@ -34,27 +34,24 @@ const run = async (): Promise<void> => {
             lines[i] = lines[i].toLowerCase()   // convert line to lowercase
             lines[i] = lines[i].replace(/\s/g, "");  // remove spaces from line
             
+            if (lines[i].startsWith("Resource Group Name:"))
+               resourceGroup = lines[i].substring(20,lines[i].length).trim()
+            
+            lines[i] = lines[i].toLowerCase()   // convert line to lowercase
+            lines[i] = lines[i].replace(/\s/g, "");  // remove spaces from line
+
             if (lines[i].startsWith("-[x]general"))
                 armTemplate = "vmss-windows-nat"
             if (lines[i].startsWith("-[x]web"))
                 armTemplate = "web-app-sql-database"
             if (lines[i].startsWith("-[x]serverless"))
                 armTemplate = "function-app"
-            if (lines[i].startsWith("-[x]pci")) {
-                applyPolicy = true
-                policyName = "Audit PCI"
-            }
-            if (lines[i].startsWith("-[x]hipaa")) {
-                applyPolicy = true
-                policyName = "Audit HITRUST/HIPAA"
-            }
         }
 
         core.setOutput('appName', appName)
         core.setOutput('armTemplate', armTemplate)
         core.setOutput('approved', 'true')
-        core.setOutput('applyPolicy', applyPolicy ? 'true' : 'false')
-        core.setOutput('policyName', policyName)
+        core.setOutput('resourceGroup', resourceGroup)
 
     } catch (error) {
         console.error(error.message)
